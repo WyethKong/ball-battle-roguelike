7d3b058f119b909b10fa5b140a5572c6f54e828b
Wyeth Kong <wyeth.kong@gmail.com>
2025-10-05 19:24:49 -0400

    feat: Create IDamageable interface

--- Ball Battle Roguelike/Assets/Scripts/CharacterController.cs
+++ Ball Battle Roguelike/Assets/Scripts/CharacterController.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterController : MonoBehaviour, IDamageable
 {
-    [SerializeField] int health = 10;
-    [SerializeField] int power = 0;
-    [SerializeField] int speed = 0;
-    [SerializeField] int magic = 0;
+    [SerializeField] int health = 10;   // The amount of damage the character can take before dying
+    [SerializeField] int power = 0;     // The amount of damage added to physical attacks 
+    [SerializeField] int magic = 0;     // The amount of damage added to magic attacks
+    [SerializeField] int speed = 0;     // The speed the character moves at
 
     Vector2 moveDirection;
+
+    public void TakeDamage(int damage)
+    {
+        health -= Mathf.Max(0, damage);
+    }
 }
