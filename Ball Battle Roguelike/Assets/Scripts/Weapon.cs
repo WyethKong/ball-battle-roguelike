93cc34ef2937afd4a7b2e94f9e28f12f905d4197
Wyeth Kong <wyeth.kong@gmail.com>
2025-10-06 13:54:54 -0400

    feat: Add weapon rotation

--- Ball Battle Roguelike/Assets/Scripts/Weapon.cs
+++ Ball Battle Roguelike/Assets/Scripts/Weapon.cs
@@ -9,14 +9,26 @@ public abstract class Weapon : MonoBehaviour
     protected Rigidbody2D rigidbody2d;
     protected Collider2D collider2d;
 
-    [SerializeField] protected int damage;
+    [SerializeField] protected double damage;
     [SerializeField] protected DamageType damageType;
+    [SerializeField] protected double speed;
     [SerializeField] protected double cooldown;
 
     protected virtual void Awake()
     {
-        owner = gameObject.GetComponentInParent<GameObject>();
+        owner = transform.parent.gameObject;
         rigidbody2d = gameObject.GetComponent<Rigidbody2D>();
         collider2d = gameObject.GetComponent<Collider2D>();
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        transform.Rotate(Vector3.forward, 10 * Time.fixedDeltaTime);
+        transform.position = owner.transform.position + (transform.up * transform.localScale.y / 2);
+    }
+
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+
     }
 }
